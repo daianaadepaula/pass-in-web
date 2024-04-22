20a9e3c3f85e0d63435c437089b990cbbbf9b71e
Daiana de Paula <daianaadepaula1@gmail.com>
2024-04-22 10:53:53 -0300

    feat: pagination app

--- src/components/attendee.list.tsx
+++ src/components/attendee.list.tsx
@@ -15,9 +15,28 @@ dayjs.locale("pt-br")
 
 export function AttendeeList() {
 	const [search, setSearch] = useState('')
+	const [page, setPage] = useState(1)
+
+	const totalPages = Math.ceil(attendees.length / 10)
 
 	function onSearchInputChanged(event: ChangeEvent<HTMLInputElement>) {
 		setSearch(event.target.value)
+	}
+
+	function goToFirstPage(){
+		setPage(1)
+	}
+
+	function goToLastPage(){
+		setPage(totalPages)
+	}
+
+	function goToPreviousPage(){
+		setPage(page - 1)
+	}
+
+	function goToNextPage(){
+		setPage(page + 1)
 	}
 
 	return (
@@ -49,7 +68,7 @@ export function AttendeeList() {
 				</thead>
 
 				<tbody>
-					{attendees.map((attendee) => {
+					{attendees.slice((page - 1) * 10, page * 10).map((attendee) => {
 						return (
 							<TableRow key={attendee.id}>
 								<TableCell>
@@ -77,23 +96,23 @@ export function AttendeeList() {
 				<tfoot>
 					<tr>
 						<TableCell colSpan={3}>
-							Mostrando 10 de 228 items
+							Mostrando 10 de {attendees.length} items
 						</TableCell>
 						<TableCell className="text-right" colSpan={3}>
 							<div className="inline-flex items-center gap-8">
-								<span>Página 1 de 23</span>
+								<span>Página {page} de {totalPages}</span>
 
 								<div className="flex gap-1.5">
-									<IconButton>
+									<IconButton onClick={goToFirstPage} disabled={page === 1}>
 										<ChevronsLeft className="size-4" />
 									</IconButton>
-									<IconButton>
+									<IconButton onClick={goToPreviousPage} disabled={page === 1}>
 										<ChevronLeft className="size-4" />
 									</IconButton>
-									<IconButton>
+									<IconButton onClick={goToNextPage} disabled={page === totalPages}>
 										<ChevronRight className="size-4" />
 									</IconButton>
-									<IconButton>
+									<IconButton onClick={goToLastPage} disabled={page === totalPages}>
 										<ChevronsRight className="size-4" />
 									</IconButton>
 								</div>
